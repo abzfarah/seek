c2f3e836765f1fa05976d060a810d445a5a04c1d
Farah Abdi <abdi.f35@gmail.com>
2017-05-07 17:31:46 +1000

    Some alignments & CSS fixes.

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import '../styles/core.scss'
 
 class AppContainer extends React.Component {
@@ -6,10 +7,19 @@ class AppContainer extends React.Component {
   render () {
     return (
         <div className="seek">
-          <div className="seek-product">
+
+          <div className="seek-product--container">
+            <div className="list">
+              <button className="btn btn-8 btn-8g">Default</button>
+              <button className="btn btn-8 btn-8g">Nike</button>
+              <button className="btn btn-8 btn-8g">Unilever</button>
+              <button className="btn btn-8 btn-8g">Ford</button>
+              <button className="btn btn-8 btn-8g">Apple</button>
+            </div>
+            <div className="seek-product">
             <div className="seek-product__info">
               <h3 className="seek-product__title">Classic Ad</h3>
-              <span className="seek-product__price highlight">$26.99</span>
+              <span className="seek-product__price hghlight">$269.99</span>
               <button className="seek-button seek-button--cart">
                 <i className="fa fa-shopping-cart"></i>
                 <span className="seek-button--cart__text">Add to cart</span></button>
@@ -20,47 +30,37 @@ class AppContainer extends React.Component {
               <span className="seek-product__add--invisible">Add to compare</span></label>
           </div>
 
-          <div className="seek-product">
-            <div className="seek-product__info">
-              <h3 className="seek-product__title">Classic Ad</h3>
-              <span className="seek-product__price highlight">$26.99</span>
-              <button className="seek-button seek-button--cart">
-                <i className="fa fa-shopping-cart"></i>
-                <span className="seek-button--cart__text">Add to cart</span></button>
+            <div className="seek-product">
+              <div className="seek-product__info">
+                <h3 className="seek-product__title">Standout Ad</h3>
+                <span className="seek-product__price highlight">$322.99</span>
+                <button className="seek-button seek-button--cart">
+                  <i className="fa fa-shopping-cart"></i>
+                  <span className="seek-button--cart__text">Add to cart</span></button>
+              </div>
+              <label className="seek-product__add">
+                <input className="check-hidden" type="checkbox" />
+                <i className="fa fa-plus"></i><i class="fa fa-check"></i>
+                <span className="seek-product__add--invisible">Add to compare</span></label>
             </div>
-            <label className="seek-product__add">
-              <input className="check-hidden" type="checkbox" />
-              <i className="fa fa-plus"></i><i class="fa fa-check"></i>
-              <span className="seek-product__add--invisible">Add to compare</span></label>
+
+            <div className="seek-product">
+              <div className="seek-product__info">
+                <h3 className="seek-product__title">Premium Ad</h3>
+                <span className="seek-product__price highlight">$349.99</span>
+                <button className="seek-button seek-button--cart">
+                  <i className="fa fa-shopping-cart"></i>
+                  <span className="seek-button--cart__text">Add to cart</span></button>
+              </div>
+              <label className="seek-product__add">
+                <input className="check-hidden" type="checkbox" />
+                <i className="fa fa-plus"></i><i class="fa fa-check"></i>
+                <span className="seek-product__add--invisible">Add to compare</span></label>
+            </div>
           </div>
 
-          <div className="seek-product">
-            <div className="seek-product__info">
-              <h3 className="seek-product__title">Classic Ad</h3>
-              <span className="seek-product__price highlight">$26.99</span>
-              <button className="seek-button seek-button--cart">
-                <i className="fa fa-shopping-cart"></i>
-                <span className="seek-button--cart__text">Add to cart</span></button>
-            </div>
-            <label className="seek-product__add">
-              <input className="check-hidden" type="checkbox" />
-              <i className="fa fa-plus"></i><i class="fa fa-check"></i>
-              <span className="seek-product__add--invisible">Add to compare</span></label>
-          </div>
 
-          <div className="seek-product">
-            <div className="seek-product__info">
-              <h3 className="seek-product__title">Classic Ad</h3>
-              <span className="seek-product__price highlight">$26.99</span>
-              <button className="seek-button seek-button--cart">
-                <i className="fa fa-shopping-cart"></i>
-                <span className="seek-button--cart__text">Add to cart</span></button>
-            </div>
-            <label className="seek-product__add">
-              <input className="check-hidden" type="checkbox" />
-              <i className="fa fa-plus"></i><i class="fa fa-check"></i>
-              <span className="seek-product__add--invisible">Add to compare</span></label>
-          </div>
+
         </div>
     )
   }
